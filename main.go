09ef06ca243a675feaea9152faa60481f1c692de
dimitar <dimitar.manev@dxc.com>
2019-04-19 16:09:40 +0300

    Initial commit

---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+    "context"
+    "errors"
+    "fmt"
+
+    pb "github.com/dmanev/shippy-service-vessel/proto/vessel"
+    "github.com/micro/go-micro"
+)
+
+func main() {
+	fmt.Println("vim-go")
+}
